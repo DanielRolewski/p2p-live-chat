416ad5299d39ce9d750790ed78a87863da89fa2d
Daniel Rolewski <daniel_rolewski@outlook.com>
2024-03-30 19:48:40 +0100

    Add room & user routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('rooms.create');
-});
+// Room routes
+Route::get('/rooms/create', [RoomController::class, 'create']);
+Route::post('/rooms', [RoomController::class, 'store']);
+Route::get('/rooms/{id}', [RoomController::class, 'show']);
 
-Route::get('/rooms', function () {
-    return view('rooms.index');
-});
+// User routes
+Route::get('/users/create', [UserController::class, 'create']);
+Route::post('/users', [UserController::class, 'store']);
