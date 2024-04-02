c3b5335d6c6bf70b77d33e1a0d45c7015ed4bf3a
Daniel Rolewski <daniel_rolewski@outlook.com>
2024-04-02 18:35:30 +0200

    Rework web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\RoomController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/rooms/create', [RoomController::class, 'create']);
+
 // Room routes
 Route::get('/rooms/create', [RoomController::class, 'create']);
+Route::get('/', [RoomController::class, 'create']);
 Route::post('/rooms', [RoomController::class, 'store']);
 Route::get('/rooms/{id}', [RoomController::class, 'show']);
 
