68afff701c8a09dffb1ca41b69fdea0b1ef2b167
Daniel Rolewski <daniel_rolewski@outlook.com>
2024-04-08 17:49:15 +0200

    Add route & channel for live-chatting feature.

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
-
 
 // Room routes
 Route::get('/rooms/create', [RoomController::class, 'create']);
@@ -14,3 +14,6 @@ Route::get('/rooms/{id}', [RoomController::class, 'show']);
 // User routes
 Route::get('/users/create', [UserController::class, 'create']);
 Route::post('/users', [UserController::class, 'store']);
+
+// Sending message routes
+Route::post('/chat/send-message', [ChatController::class, 'store'])->name('chat.send');
