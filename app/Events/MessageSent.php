573309599b7538b8df74a49f12c02622ca9c3baf
Daniel Rolewski <daniel_rolewski@outlook.com>
2024-04-08 17:51:02 +0200

    Add MessageSent event

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * @extends ShouldBroadcastNow
+ */
+class MessageSent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public string $message;
+
+    /**
+     * Create a new event instance with message from user input.
+     *
+     * @param string $message
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('chatroom'),
+        ];
+    }
+
+    /**
+     * Define broadcast class name.
+     *
+     * @return string
+     */
+    public function broadcastAs(): string
+    {
+        return 'message.sent';
+    }
+}
